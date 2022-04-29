d36227a70f72bd0048e235770082883bbbf8fb04
Vickyberg <akinnusivictor098@gmail.com>
2022-04-29 16:20:02 +0100

    new commit

---
+++ test/chapterFour/Loop.java
@@ -0,0 +1,11 @@
+package chapterFour;
+
+public class Loop {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i <= 20){
+            System.out.print(i+ " ");
+            i++;
+        }
+    }
+}
---
+++ test/chapterFour/LoopEven.java
@@ -0,0 +1,11 @@
+package chapterFour;
+
+public class LoopEven {
+    public static void main(String[] args) {
+        int i = 2;
+        for(;i <= 20;){
+            System.out.print(i+ " ");
+            i = i + 2;
+        }
+    }
+}
