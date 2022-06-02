dfa56f01b7e25b7af63fcbe2d5bda2cd9d951fef
Vickyberg <akinnusivictor098@gmail.com>
2022-06-02 11:39:57 +0100

    new commit

---
+++ src/main/java/ChapterSix/ParkingCharges.java
@@ -0,0 +1,11 @@
+package ChapterSix;
+/*
+$2.00 is charged for packing for three hours
+$0.50 is charged per extra hour
+ */
+
+public class ParkingCharges {
+    public static void main(String[] args) {
+
+    }
+}
