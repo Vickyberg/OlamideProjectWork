a1d06a2b29ce3c882d839110b857bb0c8a73552e
Victor Olamide <akinnusivictor098@gmail.com>
2022-07-15 20:43:07 +0100

    Work With Array & ArrayList

---
+++ src/main/java/ChapterSeven/CreatingAndManipulationOfArray/CommandLineArg.java
@@ -0,0 +1,27 @@
+package ChapterSeven.CreatingAndManipulationOfArray;
+
+public class CommandLineArg {
+    public static void main(String[] args) {
+        if (args.length != 3) {
+
+            System.out.printf("Error: Please re-enter the entire command, including%n" +
+                    "an array size, initial value and increment.%n");
+        }else {
+            int arrayLength = Integer.parseInt(args[0]);
+            int[] array = new int[arrayLength];
+
+            int initialValue = Integer.parseInt(args[0]);
+            int increment = Integer.parseInt(args[2]);
+
+            for (int i = 0; i < array.length; i++) {
+                array[i] = initialValue + increment * i;
+            }
+            System.out.printf("%s%8s%n","Index","Value");
+
+            for (int i = 0; i < array.length; i++) {
+                System.out.printf("%5d%8d%n",i,array[i]);
+                
+            }
+        }
+    }
+}
