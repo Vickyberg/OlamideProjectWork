078fbea8991065f446a1fc3987ca31d23df8c392
Victor Olamide <akinnusivictor098@gmail.com>
2022-07-17 04:18:21 +0100

    String

--- src/main/java/Love.java
+++
@@ -1,9 +0,0 @@
-public class Love {
-    public static void main(String[] args) {
-        System.out.println("I LOVE YOU".repeat(100));
-        System.out.println("I LOVE YOU".repeat(100));
-        System.out.println("I LOVE YOU".repeat(100));
-        System.out.println("I LOVE YOU".repeat(100));
-        System.out.println("I LOVE YOU".repeat(100));
-    }
-}
