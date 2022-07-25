1984ea890db354023c88c116d802be885541b99d
Victor Olamide <akinnusivictor098@gmail.com>
2022-07-25 12:13:31 +0100

    Banking system

--- test/tdd/BankAccount/BankTest.java
+++ test/tdd/BankAccount/BankTest.java
@@ -1,11 +1,16 @@
 package tdd.BankAccount;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class BankTest {
     Bank myBank;
     @BeforeEach
     void setMyBank(){
         myBank = new Bank("AccessBank","Yaba,Sabo");
+    }
+    @Test
+    void testThatBankCanRegisterCustomer(){
+
     }
 }
--- test/tdd/BankAccount/CustomerTest.java
+++ test/tdd/BankAccount/CustomerTest.java
@@ -3,6 +3,9 @@ package tdd.BankAccount;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class CustomerTest {
 
     Customer myCustomer;
@@ -10,5 +13,19 @@ public class CustomerTest {
     void setUp(){
         myCustomer = new Customer("Ola","Dayo","M","wrgwwef@gmail.com","Juno Africa,Akoka","2442","rgerg453");
     }
+    @Test
+    void testWeHaveACustomer(){
+        assertNotNull(myCustomer);
+    }
+
+    @Test
+    void testWeCanGetCustomerFirstName(){
+        assertEquals("Ola" ,myCustomer.getFirstName());
+    }
+    @Test
+    void testWeCanGetCustomerLastName(){
+        assertEquals("Dayo" ,myCustomer.getLastName());
+    }
+
 
 }
