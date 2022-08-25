ec523c4059c1206a4efb90aa629f43bfcb5bd9a2
Victor Olamide <akinnusivictor098@gmail.com>
2022-08-25 11:08:22 +0100

    Files and Stream

--- src/main/java/Chapter15/FileAndStream/ClassExample1.java
+++ src/main/java/Chapter15/FileAndStream/ClassExample1.java
@@ -1,7 +1,17 @@
 package Chapter15.FileAndStream;
 
+import java.io.*;
+
 public class ClassExample1 {
     public static void main(String[] args) {
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(inputStreamReader);
+        try {
+            String userResponse = reader.readLine();
+            System.out.println("LINE::::" + userResponse.toUpperCase());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 }
