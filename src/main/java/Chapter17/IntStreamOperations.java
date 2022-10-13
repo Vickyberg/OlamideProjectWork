3f6ec9829e32b0ee95320474eb65f5be1163bd14
Victor Olamide <akinnusivictor098@gmail.com>
2022-10-13 10:00:40 +0100

    IntStream Operation

--- src/main/java/Chapter17/IntStreamOperations.java
+++ src/main/java/Chapter17/IntStreamOperations.java
@@ -18,7 +18,7 @@ public class IntStreamOperations {
         System.out.printf("Sum: %d%n",IntStream.of(values).sum());
         System.out.printf("Average: %.2f%n",IntStream.of(values).average().getAsDouble());
 
-        System.out.printf("%nSum via reduce method: %d%n", IntStream.of(values).reduce(0,(x,y) -> x + y));
+        System.out.printf("%nSum via reduce method: %d", IntStream.of(values).reduce(0,(x,y) -> x + y));
         System.out.printf("%nProduct via reduce method: %d%n", IntStream.of(values).reduce((x,y) -> x * y).getAsInt());
         System.out.printf("Product via reduce method: %d%n", IntStream.of(values).reduce(0,(x,y) -> x + y));
         System.out.printf("Sum of squares via map and sum: %d%n%n",IntStream.of(values).map(x -> x *x).sum());
